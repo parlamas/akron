9b826cf7107043aac1735cfe73abb73caad7ac85
parlamas <parlamas@live.com>
2022-09-08 18:30:00 +0200

    2022-09-08-1829

--- components/Header.jsx
+++ components/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
         <div className="md:float-left block">
           <Link href="/">
             <span className="cursor-pointer font-bold text-4xl text-white">
-              Dialectics and Horistics
+              Horistics
             </span>
           </Link>
         </div>
