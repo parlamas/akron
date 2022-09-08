3efbd7159386e8fab0272b90e2174dbbb3b1b298
parlamas <parlamas@live.com>
2022-09-08 17:49:39 +0200

    2022-09-08-1749

--- components/Header.jsx
+++ components/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
         <div className="md:float-left block">
           <Link href="/">
             <span className="cursor-pointer font-bold text-4xl text-white">
-              <span style="color:gold;">Dialectics ${"&"} Horistics</span>
+              Dialectics {"&amp;"} Horistics
             </span>
           </Link>
         </div>
