07191b3db6642c9c9172c59221bc2c352db608d4
parlamas <parlamas@live.com>
2022-09-10 10:45:08 +0200

    6th

--- components/Header.jsx
+++ components/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
         <div className="md:float-left block">
           <Link href="/">
             <span className="cursor-pointer font-bold text-4xl text-white">
-              Horistics
+              Horistics (CVR: 43 10 93 24)
             </span>
           </Link>
         </div>
