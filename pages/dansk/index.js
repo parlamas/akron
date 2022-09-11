56ffea8569699e28e8c3878753f0d0edcc5a2320
parlamas <parlamas@live.com>
2022-09-11 18:14:33 +0200

    13th

---
+++ pages/dansk/index.js
@@ -0,0 +1,27 @@
+function Dansk() {
+  return (
+    <div
+      style={{
+        backgroundColor: "white",
+        color: "black",
+        fontSize: "15pt",
+        padding: 20,
+        textAlign: "justify",
+      }}
+    >
+      <h1 style={{ color: "white" }}>Dansk</h1>
+      <p>
+        {" "}
+        <br />
+        <b>(Indefinite article + noun)</b>
+      </p>
+      <ol>
+        <li>ˈHvad / er ˈdet? • En ˈstol. - Det er en ˈstol.</li>
+      </ol>
+      <br />
+      Hvad er det? <i>[ ˈvɑɑ ˈde ]</i>
+    </div>
+  );
+}
+
+export default Dansk;
