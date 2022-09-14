db1ddc5a350874b682accc1d2392adf8f66b6dfb
parlamas <parlamas@live.com>
2022-09-14 08:57:05 +0200

    14th

---
+++ pages/404.js
@@ -0,0 +1,21 @@
+function WrongUrl() {
+  return (
+    <center>
+      <div
+        style={{
+          width: "70%",
+          height: 300,
+          backgroundColor: "beige",
+          color: "black",
+          fontSize: "30pt",
+          fontWeight: "bold",
+          padding: 20,
+        }}
+      >
+        <center style={{ paddingTop: 100 }}>This URL does not exist!</center>
+      </div>
+    </center>
+  );
+}
+
+export default WrongUrl;
