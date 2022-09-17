73783e5afb855352c16839395d5aeefc2b7dadb0
parlamas <parlamas@live.com>
2022-09-17 13:59:05 +0200

    5th

--- components/Header.jsx
+++ components/Header.jsx
@@ -25,8 +25,12 @@ const Header = () => {
                   <span style={{ color: "cyan" }}>Currently: 16 Posts</span> •{" "}
                   <span style={{ color: "lime", fontSize: "9pt" }}>
                     On a desktop or laptop, the list of categories is on the top
-                    part of the screen; on a cellphone, scroll all the way down
-                    to the bottom to see the list of categories.
+                    part of the screen; to see the list of categories on a
+                    cellphone, scroll all the way down to the bottom.
+                    <br />
+                    <a href="https://dialectics.education" target="_blank">
+                      conditionals
+                    </a>
                   </span>
                 </span>
               </span>
