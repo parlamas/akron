4d4de278e1e127d4be8c000e30b2882cbd3cae18
parlamas <parlamas@live.com>
2022-09-17 14:54:42 +0200

    110th

--- components/Header.jsx
+++ components/Header.jsx
@@ -28,10 +28,12 @@ const Header = () => {
                     part of the screen; to see the list of categories on a
                     cellphone, scroll all the way down to the bottom.
                     <br />
-                    <Link href="https://dialectics.education/" passHref>
-                      <button target="_blank" rel="noopener noreferrer">
-                        conditionals
-                      </button>
+                    <Link
+                      target="_blank"
+                      href="https://dialectics.education/"
+                      passHref
+                    >
+                      <button>conditionals</button>
                     </Link>
                   </span>
                 </span>
